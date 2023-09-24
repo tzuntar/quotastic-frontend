879488ea3944f39b7fd3b6815ebac75a8f3b0e31
Tobija Žuntar <tobija.zuntar@gmail.com>
2023-09-24 22:49:42 +0200

    Show the Sign-In button on the bottom of the landing page

--- src/components/homepage/LandingHome.tsx
+++ src/components/homepage/LandingHome.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import * as API from '../../api/Api';
 import teaser_quotes from "../../assets/landing_page/teaser_quotes.png";
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import {useQueries} from "react-query";
 import {QuoteType} from "../../models/quote";
 import Quote from "../quote/Quote";
+import {routeConstants} from "../../constants/routeConstants";
+import authStore from "../../stores/auth.store";
 
 const LandingHome: React.FC = () => {
     const navigate = useNavigate();
@@ -27,18 +29,32 @@ const LandingHome: React.FC = () => {
             <img src={teaser_quotes} alt="User generated quotes" className="pt-5"/>
             <h1 className="text-3xl text-center pt-14">Explore the world of fantastic quotes</h1>
             <h2 className="text-2xl text-orange text-center pt-10">Most liked quotes</h2>
-            <p className="text-center py-5 pb-10">Most liked quotes on the platform. Sign in or create an account to like
-                and save your favorites.</p>
+            <p className="text-center py-5 pb-10">Most liked quotes on the platform. Sign in or create an account to
+                like and save your favorites.</p>
 
-            {quote.isLoading ? (
-                <p className="text-center text-alt-orange animate-pulse">Loading...</p>
-            ) : (
-                <>
-                    {quote.data?.data?.map((quote: QuoteType, index: number) => (
-                        <Quote quote={quote} key={index}/>
-                    ))}
-                </>
-            )}
+            <div className="mb-10">
+                {quote.isLoading ? (
+                    <p className="text-center text-alt-orange animate-pulse">Loading...</p>
+                ) : (
+                    <>
+                        {quote.data?.data?.map((quote: QuoteType, index: number) => (
+                            <Quote quote={quote} key={index}/>
+                        ))}
+                    </>
+                )}
+            </div>
+
+            {authStore.user === null &&
+                <div className="flex justify-center">
+                    <Link to={routeConstants.LOGIN}>
+                        <p className="inline bg-gradient-to-t from-orange to-alt-orange
+                                      py-2.5 px-8 rounded-full mt-6 text-white cursor-pointer
+                                      drop-shadow-md hover:shadow-lightener active:brightness-90">
+                            Sign In to See More
+                        </p>
+                    </Link>
+                </div>
+            }
 
         </div>
     )
