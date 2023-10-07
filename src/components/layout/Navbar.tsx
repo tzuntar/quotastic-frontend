06a18847697bcafcfe0a103c839ce98fb40b8f4a
Tobija Žuntar <tobija.zuntar@gmail.com>
2023-10-07 20:43:44 +0200

    Mobile hamburger-style menu

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -19,45 +19,143 @@ const Navbar: React.FC<Props> = ({menuActionHandler}) => {
         <header>
             {isMobile ?
                 <nav className="shadow-navbar flex items-center justify-between px-8 py-3">
-                    <div className="block lg:hidden">
-                        <button className="text-orange">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-6 w-6"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="1.5"
-                                    d="M4 6h16M4 12h16M4 18h16"
-                                />
-                            </svg>
-                        </button>
-                    </div>
-                    <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 px-8">
-                        <Link to={routeConstants.HOME}>
-                            <img src={logo} alt="Quotastic" className="w-28 md:w-40"/>
-                        </Link>
-                    </div>
-                    <Link to={routeConstants.POST_QUOTE} className="text-orange">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-6 w-6"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="1.5"
-                                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-                            />
-                        </svg>
-                    </Link>
+                    {!menuVisible ?
+                        <>
+                            <div className="block lg:hidden">
+                                <button className="text-orange"
+                                        onClick={() => setMenuVisible(!menuVisible)}>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-6 w-6"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="1.5"
+                                            d="M4 6h16M4 12h16M4 18h16"
+                                        />
+                                    </svg>
+                                </button>
+                            </div>
+                            <div
+                                className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 px-8">
+                                <Link to={routeConstants.HOME}>
+                                    <img src={logo} alt="Quotastic" className="w-28 md:w-40"/>
+                                </Link>
+                            </div>
+                            <Link to={routeConstants.POST_QUOTE} className="text-orange">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-6 w-6"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="1.5"
+                                        d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                                    />
+                                </svg>
+                            </Link>
+                        </>
+                        :
+                        <>
+                            <div className="flex-none w-full">
+                                <div className="block pt-4">
+                                    <button className="text-orange"
+                                            onClick={() => setMenuVisible(!menuVisible)}>
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            className="h-6 w-6"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke="currentColor"
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="1.5"
+                                                d="M6 18L18 6M6 6l12 12"
+                                            />
+                                        </svg>
+
+                                    </button>
+                                </div>
+                                <div className="block pt-4">
+                                    <Link to={routeConstants.USER_PROFILE}>
+                                        <div className="flex flex-row space-x-4 align-middle items-center">
+                                            <img src={authStore?.user?.avatarUrl ? authStore.user.avatarUrl : default_avatar}
+                                                 alt="Profile" className="w-8 h-8 drop-shadow-sm"/>
+                                            <p>{authStore?.user?.firstName} {authStore?.user?.lastName}</p>
+                                        </div>
+                                    </Link>
+                                </div>
+                                <div className="block pt-6 flex flex-row justify-between">
+                                    <Link to={routeConstants.HOME}>
+                                        Home
+                                    </Link>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-4 w-4"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            d="M12 6l8 8-8 8"
+                                        />
+                                    </svg>
+                                </div>
+                                <div className="block pt-6 flex flex-row justify-between">
+                                    <Link to={routeConstants.USER_PREFERENCES}>
+                                        Preferences
+                                    </Link>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-4 w-4"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            d="M12 6l8 8-8 8"
+                                        />
+                                    </svg>
+                                </div>
+                                <div className="block py-6 flex flex-row justify-between">
+                                    <Link to={routeConstants.LOGOUT}>
+                                        <p className="text-orange">Sign Out</p>
+                                    </Link>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-4 w-4 text-orange"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            d="M12 6l8 8-8 8"
+                                        />
+                                    </svg>
+                                </div>
+                            </div>
+                        </>
+
+                    }
                 </nav>
                 :
                 <nav className="flex items-center justify-between p-4 px-8">
