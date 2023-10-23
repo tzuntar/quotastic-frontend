8a2438354ce80b43538a73373caf16cf98844daa
Tobija Žuntar <tobija.zuntar@gmail.com>
2023-10-23 21:06:34 +0200

    Fix several bugs causing user profile pages to fail loading or load incorrect data

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -9,15 +9,20 @@ const UserProfilePage: React.FC = () => {
 
     const user = useQuery({
         queryKey: ['fetchUserById', 1],
-        queryFn: () => API.fetchUserById(userId!)
+        queryFn: () => API.fetchUserById(userId!),
+        keepPreviousData: false
     });
 
-    return (user
-            ? <UserProfile user={user.data?.data}/>
-            : <div className="p-10 pb-28">
-                <p className="text-center">User Not Found</p>
-            </div>
-    );
+    return user.isLoading
+        ? <div className="p-10">
+            <p className="text-center text-alt-orange animate-pulse">Loading...</p>
+        </div>
+        : (user.isFetched && user
+                ? <UserProfile user={user.data?.data}/>
+                : <div className="p-10 pb-28">
+                    <p className="text-center">User Not Found</p>
+                </div>
+        );
 }
 
 export default UserProfilePage;
